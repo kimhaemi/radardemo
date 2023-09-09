9666a483469f655906c1c89ad5a41f5451f0aac6
haems <imeahmik@gmail.com>
2023-09-09 18:00:08 +0900

    파일명 패턴

--- src/main/java/kr/or/kimsn/radardemo/common/SftpUtil.java
+++ src/main/java/kr/or/kimsn/radardemo/common/SftpUtil.java
@@ -1,14 +1,10 @@
 package kr.or.kimsn.radardemo.common;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.Date;
 import java.util.Properties;
 import java.util.Vector;
 
-import org.h2.mvstore.DataUtils;
 import org.springframework.stereotype.Component;
-import org.springframework.util.unit.DataUnit;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,38 +30,36 @@ public class SftpUtil {
 
     private final ReceiveSettingRepository receiveSettingRepository;
 
-    //sftp 서버 연결
-    public boolean open(String host, String id, String password, int port) { 
+    // sftp 서버 연결
+    public boolean open(String host, String id, String password, int port) {
         boolean result = false;
 
-        //JSch 객체를 생성
+        // JSch 객체를 생성
         jSch = new JSch();
 
-        
         try {
-            //privateKey 인증
+            // privateKey 인증
             // jSch.addIdentity(privateKey);
 
-            //JSchSession 객체를 생성 (사용자 이름, 접속할 호스트, 포트 전달)
+            // JSchSession 객체를 생성 (사용자 이름, 접속할 호스트, 포트 전달)
             Session session = jSch.getSession(id, host, port);
             session.setPassword(password);
 
-            //기타 설정 적용
+            // 기타 설정 적용
             Properties config = new Properties();
             config.put("StrictHostKeyChecking", "no");
             session.setConfig(config);
 
-            //접속
+            // 접속
             session.connect();
 
-            //sftp 채널 열기 및 접속
+            // sftp 채널 열기 및 접속
             channel = session.openChannel("sftp");
-            channel.connect(5000); //5초
+            channel.connect(5000); // 5초
             result = true;
-            
+
             // 채널을 FTP 용 채널 객체로 캐스팅
             channelSftp = (ChannelSftp) channel;
-
 
         } catch (JSchException e) {
             result = false;
@@ -74,51 +68,53 @@ public class SftpUtil {
             // log.error("SFTP: server connect failed.");
         }
 
-
         return result;
     }
-    
-    //sftp 서버 연결 종료
-    private void close() { 
-        
+
+    // sftp 서버 연결 종료
+    private void close() {
+
     }
 
     /**
      * 디렉토리( or 파일) 존재 여부
+     * 
      * @param path 디렉토리 (or 파일)
      * @return
      */
     public boolean fileExists(String path, String siteCd, String dataKind) {
         System.out.println("[파일 경로] : " + path);
-        
 
-        //1. 자료감시설정 on
-        //2. 파일 패턴으로 파일명 찾기
+        String fileName = "";
+
+        // 1. 자료감시설정 on
+        // 2. 파일 패턴으로 파일명 찾기
         try {
             ReceiveSettingDto rsDto = receiveSettingRepository.findByDataKindAndPermittedWatchAndStatus(dataKind, 1, 1);
             System.out.println(rsDto);
             String filePattern = rsDto.getFilename_pattern();
             String timeZone = rsDto.getTime_zone();
             String dateTime = "";
-            
-            if(timeZone.equals("K")) dateTime = FormatDateUtil.formatDate("yyyyMMddHHmm", new Date());
-            if(timeZone.equals("U")) dateTime = FormatDateUtil.formatDate("yyyyMMddHHmm", new Date());
-            System.out.println(FormatDateUtil.changeKstToUtc(new Date()));
-        
-            System.out.println("[date Time] : " + dateTime);
 
-            String fileName = filePattern.replace("%SITE%", siteCd).replace("%yyyyMMddHHmm%", dateTime);
+            if (timeZone.equals("K"))
+                dateTime = FormatDateUtil.formatDate("yyyyMMddHHmm", new Date());
+            if (timeZone.equals("U"))
+                dateTime = FormatDateUtil.formatDate("yyyyMMddHHmm", new Date());
+            System.out.println(FormatDateUtil.changeKstToUtc(new Date()));
+
+            System.out.println("[date Time] : " + dateTime);
+            System.out.println("[siteCd] : " + siteCd);
+
+            fileName = filePattern.replace("%site%", siteCd).replace("%yyyyMMddHHmm%", dateTime);
             System.out.println("[파일 패턴] : " + filePattern);
             System.out.println("[파일 명] : " + fileName);
         } catch (Exception e) {
             System.out.println("파일 존재여부 확인 에러 - " + e);
         }
 
-        
-
         Vector res = null;
         try {
-            res = channelSftp.ls(path);
+            res = channelSftp.ls(path + "/" + fileName);
             System.out.println("[파일 존재여부 확인]" + res.size());
         } catch (SftpException e) {
             if (e.id == ChannelSftp.SSH_FX_NO_SUCH_FILE) {
@@ -128,11 +124,11 @@ public class SftpUtil {
         return res != null && !res.isEmpty();
     }
 
-    //파일 사이즈
+    // 파일 사이즈
     public boolean fileSize(String path, Long file_size_min, Long file_size_max) {
         System.out.println("[파일 경로] : " + path);
         // System.out.println("[파일 명] : " + file.getName());
-        
+
         Vector res = null;
         try {
             res = channelSftp.ls(path);
@@ -144,6 +140,5 @@ public class SftpUtil {
         }
         return res != null && !res.isEmpty();
     }
-    
-    
+
 }
