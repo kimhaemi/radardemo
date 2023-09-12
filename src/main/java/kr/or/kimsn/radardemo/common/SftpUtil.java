d8f2cae9e6c9f1263e21841ed434bfaa0d67ab97
haems <imeahmik@gmail.com>
2023-09-12 16:51:32 +0900

    file size ok

--- src/main/java/kr/or/kimsn/radardemo/common/SftpUtil.java
+++ src/main/java/kr/or/kimsn/radardemo/common/SftpUtil.java
@@ -1,9 +1,11 @@
 package kr.or.kimsn.radardemo.common;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,11 +15,11 @@ import java.util.Vector;
 
 import org.springframework.context.annotation.Configuration;
 
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
@@ -35,6 +37,7 @@ public class SftpUtil {
     private Session session;
     private Channel channel;
     private ChannelSftp channelSftp;
+    private ChannelExec channelExec;
 
     // sftp 서버 연결
     public boolean open(String host, String id, String password, int port) {
@@ -63,7 +66,7 @@ public class SftpUtil {
 
             // sftp 채널 열기 및 접속
             channel = session.openChannel("sftp");
-            channel.connect(connectTimeOut*1000); // 10초
+            channel.connect(connectTimeOut * 1000); // 10초
             result = true;
 
             // 채널을 FTP 용 채널 객체로 캐스팅
@@ -87,8 +90,11 @@ public class SftpUtil {
         if (channel != null) {
             channel.disconnect();
         }
-        if(channelSftp != null) {
-        	channelSftp.disconnect();
+        if (channelSftp != null) {
+            channelSftp.disconnect();
+        }
+        if (channelExec != null) {
+            channelExec.disconnect();
         }
     }
 
@@ -98,7 +104,8 @@ public class SftpUtil {
      * @param path 디렉토리 (or 파일)
      * @return
      */
-    public boolean fileExists(String path, String fileName, String siteCd, String dataKind, String filePattern, String timeZone) {
+    public boolean fileExists(String path, String fileName, String siteCd, String dataKind, String filePattern,
+            String timeZone) {
         System.out.println("[파일 경로] : " + path);
 
         Vector res = null;
@@ -117,34 +124,17 @@ public class SftpUtil {
     public Long fileSize(String path, String fileName, Long file_size_min, Long file_size_max) {
         System.out.println("[file size check]");
         Long fileSize = 0L;
-        // BufferedInputStream bis = null;
-        Vector res = null;
         try {
-            res = channelSftp.ls(path+"/"+fileName);
+            fileSize = channelSftp.lstat(path + "/" + fileName).getSize();
 
-            System.out.println("res : " + res.get(0));
-            System.out.println("res : " + res.elements());
-
-            // FileChannel fileChannel = FileChannel.open(paths);
-
-            // System.out.println("fileChannel :::: " + fileChannel.size());
-
-            // channelSftp.cd(path);
-
-			// File file = new File(fileName);
-            // System.out.println("" + file.size());
-
-            // is = channelSftp.get(path + "/" + fileName);
-            // System.out.println("[파일 size]" + is);
+            // System.out.println("[파일 size] : " + fileSize);
         } catch (SftpException e) {
             if (e.id == ChannelSftp.SSH_FX_NO_SUCH_FILE) {
                 return 0L;
             }
-        // } catch (IOException e) {
-        //     System.out.println("file size error : " + e);
-        //     return 0L;
-        //     // if (e.id == ChannelSftp.SSH_FX_NO_SUCH_FILE) {
-        //     // }
+            // } catch (IOException e) {
+            // System.out.println("file size error : " + e);
+            // return 0L;
         }
         return fileSize;
     }
--- src/main/java/kr/or/kimsn/radardemo/process/StepOneProcess.java
+++ src/main/java/kr/or/kimsn/radardemo/process/StepOneProcess.java
@@ -17,17 +17,17 @@ import lombok.RequiredArgsConstructor;
 public class StepOneProcess extends Thread {
 
     private final QueryService queryService;
-    
+
     private List<StationDto> srDto;
-    
+
     @Override
     public void run() {
-        String gubunStr = ""; //데몬 구분
-        String dataKindStr = ""; //데이터 종류
-        String dataType = "NQC"; //데이터 타입
-        String recv_condition = ""; //항목별 자료 수신 상태
-        String recv_condition_data = ""; //항목별 자료 수신 상태
-        int srCnt = 0; //for문 site 갯수
+        String gubunStr = ""; // 데몬 구분
+        String dataKindStr = ""; // 데이터 종류
+        String dataType = "NQC"; // 데이터 타입
+        String recv_condition = ""; // 항목별 자료 수신 상태
+        String recv_condition_data = ""; // 항목별 자료 수신 상태
+        int srCnt = 0; // for문 site 갯수
 
         String currentTime = FormatDateUtil.formatDate("yyyy-MM-dd HH:mm:ss", new Date());
         String apply_time = currentTime;
@@ -40,57 +40,66 @@ public class StepOneProcess extends Thread {
         int sms_send = 0;
         String file_name = "";
         Long file_size = 0L;
-        
+
         int gubun = Integer.parseInt(DataCommon.getInfoConf("siteInfo", "data_kind"));
         System.out.println("[데몬 구분 int] : " + gubun);
-        
-        if(gubun == 1) gubunStr = "대형";
-        if(gubun == 2) gubunStr = "소형";
-        if(gubun == 3) gubunStr = "공항";
+
+        if (gubun == 1)
+            gubunStr = "대형";
+        if (gubun == 2)
+            gubunStr = "소형";
+        if (gubun == 3)
+            gubunStr = "공항";
         System.out.println("[데몬 구분 str] : " + gubunStr);
 
-        if(gubun == 1) dataKindStr = "RDR";
-        if(gubun == 2) dataKindStr = "SDR";
-        if(gubun == 3) dataKindStr = "TDWR";
+        if (gubun == 1)
+            dataKindStr = "RDR";
+        if (gubun == 2)
+            dataKindStr = "SDR";
+        if (gubun == 3)
+            dataKindStr = "TDWR";
         System.out.println("[데이터 종류 str] : " + dataKindStr);
 
         String mode = DataCommon.getInfoConf("ipInfo", "mode");
         System.out.println("[mode] : " + mode);
 
-        if (mode.equals("test")) srCnt = 1;
+        if (mode.equals("test"))
+            srCnt = 1;
         if (!mode.equals("test")) {
             srDto = queryService.getStation(gubun);
             srCnt = srDto.size();
         }
-        
+
         System.out.println("[radar size] : " + srCnt);
 
-        for(int a=0; a< srCnt; a++){
+        for (int a = 0; a < srCnt; a++) {
             String site_cd = "";
-            if(srCnt == 1) site_cd = "TEST";
-            if(srCnt > 1) site_cd = srDto.get(a).getSiteCd();
+            if (srCnt == 1)
+                site_cd = "TEST";
+            if (srCnt > 1)
+                site_cd = srDto.get(a).getSiteCd();
 
             int port = Integer.parseInt(DataCommon.getInfoConf("ipInfo", "PORT"));
-            String site_ip = DataCommon.getInfoConf("ipInfo", site_cd+"_IP");
-            String site_username = DataCommon.getInfoConf("ipInfo", site_cd+"_ID");
-            String site_pwd = DataCommon.getInfoConf("ipInfo", site_cd+"_PASSWORD");
+            String site_ip = DataCommon.getInfoConf("ipInfo", site_cd + "_IP");
+            String site_username = DataCommon.getInfoConf("ipInfo", site_cd + "_ID");
+            String site_pwd = DataCommon.getInfoConf("ipInfo", site_cd + "_PASSWORD");
 
             try {
 
-                System.out.println("[" + site_cd+" site 접속 정보]");
-                System.out.println("[" + site_cd+"_port] : "+port);
-                System.out.println("[" + site_cd+"_ip] : "+site_ip);
-                System.out.println("[" + site_cd+"_username] : "+site_username);
-                System.out.println("[" + site_cd+"_pwd] : "+site_pwd);
-                
-                //site 접속
+                System.out.println("[" + site_cd + " site 접속 정보]");
+                System.out.println("[" + site_cd + "_port] : " + port);
+                System.out.println("[" + site_cd + "_ip] : " + site_ip);
+                System.out.println("[" + site_cd + "_username] : " + site_username);
+                System.out.println("[" + site_cd + "_pwd] : " + site_pwd);
+
+                // site 접속
                 SftpUtil sftp = new SftpUtil();
                 boolean sftpConnect = sftp.open(site_ip, site_username, site_pwd, port);
 
                 System.out.println("[접속 유무] : " + sftpConnect);
-                
-                //접속 O
-                if(sftpConnect){
+
+                // 접속 O
+                if (sftpConnect) {
                     String file_path = DataCommon.getInfoConf("siteInfo", "rdr_path");
 
                     ReceiveSettingDto rsDto = queryService.getrRceiveSetting(dataKindStr);
@@ -101,82 +110,99 @@ public class StepOneProcess extends Thread {
 
                     String dateTime = FormatDateUtil.formatDate("yyyyMMddHHmm", new Date());
 
-                    if (Integer.parseInt(dateTime.substring(dateTime.length() -1, dateTime.length())) < 5)
-                        dateTime = dateTime.substring(0, dateTime.length() -1) + "0";
-                    if (Integer.parseInt(dateTime.substring(dateTime.length() -1, dateTime.length())) > 5)
-                        dateTime = dateTime.substring(0, dateTime.length() -1) + "5";
-                    
+                    if (Integer.parseInt(dateTime.substring(dateTime.length() - 1, dateTime.length())) < 5)
+                        dateTime = dateTime.substring(0, dateTime.length() - 1) + "0";
+                    if (Integer.parseInt(dateTime.substring(dateTime.length() - 1, dateTime.length())) > 5)
+                        dateTime = dateTime.substring(0, dateTime.length() - 1) + "5";
+
                     System.out.println("[date Time] : " + dateTime);
                     System.out.println("[site_cd] : " + site_cd);
 
-                    //파일 패턴으로 파일명 찾기
+                    // 파일 패턴으로 파일명 찾기
                     file_name = filePattern.replace("%site%", site_cd).replace("%yyyyMMddHHmm%", dateTime);
                     System.out.println("[파일 패턴] : " + filePattern);
                     System.out.println("[파일 명] : " + file_name);
 
-                    //자료감시 설정 on
-                    if(rsDto.getPermittedWatch() == 1){
+                    // 자료감시 설정 on
+                    if (rsDto.getPermittedWatch() == 1) {
                         System.out.println("[자료감시 설정 on]");
-                        boolean file_exists = sftp.fileExists(file_path, file_name, site_cd, dataKindStr, filePattern, timeZone);
+                        boolean file_exists = sftp.fileExists(file_path, file_name, site_cd, dataKindStr, filePattern,
+                                timeZone);
                         System.out.println("[파일존재유무] : " + file_exists);
-                        
-                        //파일 O (ORDI - file_ok)
-                        if(file_exists){
+
+                        // 파일 O (ORDI - file_ok)
+                        if (file_exists) {
                             System.out.println("[파일 O]");
                             Long file_size_min = Long.parseLong(DataCommon.getInfoConf("siteInfo", "file_size_min"));
                             Long file_size_max = Long.parseLong(DataCommon.getInfoConf("siteInfo", "file_size_max"));
 
                             file_size = sftp.fileSize(file_path, file_name, file_size_min, file_size_max);
-                            
-                            //파일 품질 정상 (ORDI - filesize_ok)
-                            if(file_size > file_size_min && file_size < file_size_max){
+                            System.out.println("[file size] : " + file_size);
+
+                            Long kb = file_size / 1024;
+                            System.out.println("[kb] : " + kb);
+
+                            // 파일 품질 정상 (ORDI - filesize_ok)
+                            if (kb > file_size_min) {
                                 recv_condition = "ORDI";
                                 codedtl = "filesize_ok";
                                 recv_condition_data = "RECV";
-                                System.out.println("[자료 수신 (ORDI - filesize_ok) query insert table1 - receive_condition]");
+                                System.out.println(
+                                        "[자료 수신 (ORDI - filesize_ok) query insert table1 - receive_condition]");
                                 System.out.println("[자료 수신 (ORDI - filesize_ok) query insert table2 - receive_data]");
                             } else {
-                            //파일 품질 이상 (WARN - filesize_no)
+                                // 파일 품질 이상 (WARN - filesize_no)
                                 recv_condition = "WARN";
                                 codedtl = "filesize_no";
                                 recv_condition_data = "MISS";
-                                System.out.println("[파일 품질 이상 (WARN - filesize_no) query insert table1 - receive_condition]");
-                                System.out.println("[파일 품질 이상 (WARN - filesize_no) query insert table2 - receive_data]");
+                                System.out.println(
+                                        "[파일 품질 이상 (WARN - filesize_no) query insert table1 - receive_condition]");
+                                System.out
+                                        .println("[파일 품질 이상 (WARN - filesize_no) query insert table2 - receive_data]");
                             }
-                        
+
                         } else {
-                        //파일 X (WARN - file_no)
+                            // 파일 X (WARN - file_no)
                             recv_condition = "WARN";
                             codedtl = "file_no";
                             System.out.println("[자료 미수신 (WARN - file_no) query insert table1 - receive_condition]");
-                            // queryService.InsReceiveCondition(site_cd, dataKindStr, dataType, recv_condition, apply_time, last_check_time, sms_send, status, codedtl);
+                            // queryService.InsReceiveCondition(site_cd, dataKindStr, dataType,
+                            // recv_condition, apply_time, last_check_time, sms_send, status, codedtl);
 
                             System.out.println("[자료 미수신 (WARN - file_no) query insert table2 - receive_data]");
                             recv_condition_data = "MISS";
-                            // queryService.InsReceiveData(dataKindStr, site_cd, dataType, data_time, data_kst, data_kst, recv_condition, recv_condition_check_time, file_name, file_size, codedtl);
+                            // queryService.InsReceiveData(dataKindStr, site_cd, dataType, data_time,
+                            // data_kst, data_kst, recv_condition, recv_condition_check_time, file_name,
+                            // file_size, codedtl);
                         }
 
                     } else {
-                        //자료감시 설정 off
+                        // 자료감시 설정 off
                         System.out.println("[자료감시 설정 off]");
                     }
-                    
+
                 } else {
-                //접속 X (TOTA)
+                    // 접속 X (TOTA)
                     recv_condition = "TOTA";
                     codedtl = "file_no";
-                    
+
                     System.out.println("[접속 실패 query insert table1 - receive_condition]");
-                    // queryService.InsReceiveCondition(site_cd, dataKindStr, dataType, recv_condition, apply_time, last_check_time, sms_send, status, codedtl);
-                    
+                    // queryService.InsReceiveCondition(site_cd, dataKindStr, dataType,
+                    // recv_condition, apply_time, last_check_time, sms_send, status, codedtl);
+
                     System.out.println("[접속 실패 query insert table2 - receive_data]");
                     recv_condition_data = "MISS";
-                    // queryService.InsReceiveData(dataKindStr, site_cd, dataType, data_time, data_kst, data_kst, recv_condition, recv_condition_check_time, file_name, file_size, codedtl);
+                    // queryService.InsReceiveData(dataKindStr, site_cd, dataType, data_time,
+                    // data_kst, data_kst, recv_condition, recv_condition_check_time, file_name,
+                    // file_size, codedtl);
 
                 }
 
-                // queryService.InsReceiveCondition(site_cd, dataKindStr, dataType, recv_condition, apply_time, last_check_time, sms_send, status, codedtl);
-                // queryService.InsReceiveData(dataKindStr, site_cd, dataType, data_time, data_kst, data_kst, recv_condition, recv_condition_check_time, file_name, file_size, codedtl);
+                // queryService.InsReceiveCondition(site_cd, dataKindStr, dataType,
+                // recv_condition, apply_time, last_check_time, sms_send, status, codedtl);
+                // queryService.InsReceiveData(dataKindStr, site_cd, dataType, data_time,
+                // data_kst, data_kst, recv_condition, recv_condition_check_time, file_name,
+                // file_size, codedtl);
             } catch (Exception e) {
                 // log.debug("error : " + e);
                 System.out.println("StepOneProcess run Error - " + e);
