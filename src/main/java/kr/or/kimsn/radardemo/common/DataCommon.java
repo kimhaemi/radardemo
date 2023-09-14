ce3f16a37c2ae7490e1063fd72d1c173c9537de3
haems <imeahmik@gmail.com>
2023-09-14 22:32:59 +0900

    conf file

--- src/main/java/kr/or/kimsn/radardemo/common/DataCommon.java
+++ src/main/java/kr/or/kimsn/radardemo/common/DataCommon.java
@@ -5,25 +5,30 @@ import java.util.Properties;
 
 public class DataCommon {
 
-    private final static String siteInfo = "./conf/siteInfoSetting.conf";
-    private final static String ipInfo = "./../ipConf/siteIPInfoSetting.conf";
+    // private final static String siteInfo = "./conf/siteInfoSetting.conf";
+    // private final static String ipInfo = "./../ipConf/siteIPInfoSetting.conf";
+    private final static String siteInfo = "/home/watcher/deamon/RDR/conf/siteInfoSetting.conf";
+    private final static String ipInfo = "/home/watcher/ipConf/siteIPInfoSetting.conf";
 
-    public static String getInfoConf(String infoStr, String tp){
+    public static String getInfoConf(String infoStr, String tp) {
         String confInfo = "";
+
         FileInputStream ipInfoIn = null;
 
         try {
             Properties ipInfoProps = new Properties();
-            if(infoStr.equals("siteInfo")) ipInfoIn = new FileInputStream(siteInfo);
-            if(infoStr.equals("ipInfo")) ipInfoIn = new FileInputStream(ipInfo);
-            
+            if (infoStr.equals("siteInfo"))
+                ipInfoIn = new FileInputStream(siteInfo);
+            if (infoStr.equals("ipInfo"))
+                ipInfoIn = new FileInputStream(ipInfo);
+
             ipInfoProps.load(ipInfoIn);
             confInfo = ipInfoProps.getProperty(tp);
-            
+
         } catch (Exception e) {
             System.out.println("사이트 Info 환경설정 정보 읽어오기 실패!!");
         }
         return confInfo;
     }
-    
+
 }
